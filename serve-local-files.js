6182bac660750213ca77df1227aa5d2f844698f3
Burns <brian.burns@unbounce.com>
2022-04-01 09:32:59 -0700

    Set up manifest

--- serve-local-files.js
+++ serve-local-files.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 8081;
+const PORT = 8082;
 const package = require('./package.json');
 
 app.use(cors());
