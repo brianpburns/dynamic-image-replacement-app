7a4b4441e66d782e17a8a228e0136d318023310e
Burns <brian.burns@unbounce.com>
2022-04-01 09:33:35 -0700

    Rename directory

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { registerComponent } from 'ub-shared';
 
-import { Component } from './hello-world';
+import { Component } from './dynamic-image-replacement';
 
 /**
  * Feel free to register whatever you need to export.
