6b0906aaf9428d7f8505709ddee4c0866ef4d5d8
Burns <brian.burns@unbounce.com>
2022-04-04 10:46:09 -0700

    Add back placeholder image on delete

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -2,13 +2,12 @@ import { component, Schema } from 'ub-shared';
 
 import DynamicImageReplacement from './components/dynamic-image-replacement';
 import { migrations } from './migrations';
+import { placeholderImageSrc } from './utils/placeholder-image-src';
 
 const schema = Schema.object({
   queryParam: Schema.string(),
   numOptions: Schema.number().default(1),
-  defaultSrc: Schema.string().default(
-    'https://user-assets-unbounce-com.s3.amazonaws.com/bb365067-415a-4ea4-ab60-9a8543ed09a5/805bfb1a-9d37-438b-b11e-269d5e3def17/575x375.original.jpg',
-  ),
+  defaultSrc: Schema.string().default(placeholderImageSrc),
   defaultAlt: Schema.string(),
   options: Schema.array(
     Schema.object({
---
+++ src/component/utils/placeholder-image-src.ts
@@ -0,0 +1,2 @@
+export const placeholderImageSrc =
+  'https://user-assets-unbounce-com.s3.amazonaws.com/bb365067-415a-4ea4-ab60-9a8543ed09a5/805bfb1a-9d37-438b-b11e-269d5e3def17/575x375.original.jpg';
