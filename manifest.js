4ce46755ffe728739a764fcfd0d07ac47fa18013
Burns <brian.burns@unbounce.com>
2022-05-13 09:32:20 -0700

    Add js manifest

---
+++ manifest.js
@@ -0,0 +1,14 @@
+export default {
+  appId: '@burns/dynamicImageReplacement',
+  version: '0.1',
+  name: 'Dynamic Image Replacement',
+  isBeta: true,
+  isSwappable: true,
+  type: 'utility',
+  description: 'Replace images using query params',
+  iconUrl:
+    'https://app-assets-unbounce-com.s3.amazonaws.com/content-smart-builder-apps/dynamic-image-replacement/icon.svg',
+  categories: [],
+  files: [],
+  externalScripts: ['http://localhost:8083/index.js'],
+};
